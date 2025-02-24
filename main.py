3b37fefcc754bfb8cf7dd04a585d75a672027d6b
R. M. <56444052+Rena006@users.noreply.github.com>
2025-02-24 19:06:29 -0300

    Feat: Added WebSocket and updated Discord notification workflow

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import os
-
-import requests
-from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.middleware.cors import CORSMiddleware
-from firebase_admin import credentials, firestore, initialize_app
-from pydantic import BaseModel
-
-from firebase import save_to_firestore
-from models import ImageRequest
 from openAI.client import OpenAIClient
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+from models import ImageRequest
+from firebase import save_to_firestore
 from prompts.prompts import HAND_PROMPT_3
 
 load_dotenv()
@@ -20,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"], 
     allow_headers=["*"],
 )
 
@@ -28,11 +24,15 @@ IMAGE_PROMPT = HAND_PROMPT_3
 MODEL = "gpt-4o-mini"
 
 API_KEY = os.getenv("OPENAI_API_KEY")
-RASP_API_URL = str(os.getenv("RASP_API_URL"))
+RASP_API_URL = os.getenv("RASP_API_URL")
 
+@app.get("/")
+def ping():
+    return {"response": "OK"}
 
 @app.websocket("/ws")
 async def websocket_server(websocket: WebSocket):
+    """WebSocket server to receive real-time keypoints."""
     await websocket.accept()
     try:
         while True:
@@ -42,32 +42,6 @@ async def websocket_server(websocket: WebSocket):
         print("Client disconnected")
     finally:
         await websocket.close()
-
-
-cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
-cred = credentials.Certificate(cred_path)
-initialize_app(cred)
-db = firestore.client()
-
-
-@app.get("/")
-def ping():
-    return {"response": "OK"}
-
-
-# Please, create the models in another location
-class image(BaseModel):
-    data: str
-
-
-def save_to_firestore(data):
-    try:
-        doc_ref = db.collection("responses").document()
-        doc_ref.set(data)
-        print("Data saved successfully in Firestore")
-    except Exception as e:
-        print(f"Error saving data to Firestore: {e}")
-
 
 @app.post("/upload-image")
 def upload_image(request: ImageRequest):
@@ -79,9 +53,8 @@ def upload_image(request: ImageRequest):
         cleaned_response = response.replace("```json", "").replace("```", "").strip()
         print(cleaned_response)
         save_to_firestore({"response": cleaned_response})
-
+        
         return {"response": cleaned_response}
-
 
 def post_data(instructions):
     url = RASP_API_URL
